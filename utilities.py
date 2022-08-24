9f7007d1f8ce02d0a5e8322fc8438e189e93f9ca
Kate Martian <kate.martianova@doriclenses.com>
2022-08-24 11:09:50 -0400

    Create utilities.py
    Add functions to load and save data to doric file
    
    Co-Authored-By: katemartian <20586089+katemartian@users.noreply.github.com>

---
+++ utilities.py
@@ -0,0 +1,279 @@
+import os
+import cv2
+import h5py
+import numpy as np
+from typing import Any, Dict, List, Optional, Tuple, Union, Callable
+
+def load_attributes(
+    f: h5py.File,
+    path: str
+) -> dict:
+    
+    params = {}
+    
+    for key, value in f[path].attrs.items():
+
+        if type(value) == np.ndarray and value.shape == (2,) and value.size == 2:
+            params[key] = tuple(value)
+        elif type(value) == np.ndarray:
+            params[key] = value.tolist()
+        else:
+            params[key] = value
+        
+    return params
+
+
+def get_frequency(
+    fname: str,
+    path: str
+) -> float:
+
+    with h5py.File(fname, 'r') as f:
+        t = np.array(f[path])
+
+    dt = np.diff(t)
+    T = np.round(np.median(dt),10)
+
+    return 1/T
+
+
+def get_dims(
+    fname: str,
+    path: str
+) -> Tuple[int, Tuple[int, int]]:
+
+    with h5py.File(fname, 'r') as f:
+        shape = np.array(f[path]).shape
+
+    return shape[-1], shape[:-1]
+
+
+def save_images(
+    images: np.ndarray,
+    time_: np.array,
+    f: h5py.File, 
+    path: str, 
+    bits_count: int = 16,
+    qt_format: int = 28,
+):
+    """
+    Saves images and time vector in HDF file as 'ImagesStack' and 'Time' 
+    datasets in `path` group. Saves images width, height, `bits_count`, and
+    `qt_format` as 'ImagesStack' dataset attribute
+    Parameters
+    ----------
+    images : np.ndarray
+        3D images stack, with shape (frame, height, width).
+    time_ : np.array
+        1D vector of timestamps
+    f : h5py.File
+        Opened HDF file where the information should be saved
+    path  : str
+        Group path in the HDF file
+    bits_count : int
+        Bits depth of images
+    qt_format : int
+        QImage_Format, necessary to display images in DNS. For reference, please
+        see https://doc.qt.io/qt-6/qimage.html
+
+    Returns
+    -------
+
+    Raises
+    ------
+
+    """
+    
+    #images = images.astype(np.uint16)
+    
+    duration = images.shape[0]
+    height = images.shape[1]
+    width = images.shape[2]
+
+    try:
+        dataset = f.create_dataset(path+'ImagesStack', (height,width,duration), dtype='uint16', 
+                                  chunks=(height,width,1), maxshape=(height,width,None))
+    except:
+        del f[path+'ImagesStack']
+        dataset = f.create_dataset(path+'ImagesStack', (height,width,duration), dtype='uint16', 
+                                  chunks=(height,width,1), maxshape=(height,width,None))
+        
+
+    for i, image in enumerate(images):
+        dataset[:,:,i] = image
+        print(i)
+        
+    try:
+        f.create_dataset(path+'Time', data=time_, dtype='float64', chunks=True, maxshape=None)
+    except:
+        del f[path+'Time']
+        f.create_dataset(path+'Time', data=time_, dtype='float64', chunks=True, maxshape=None)
+
+    f[path+'ImagesStack'].attrs['BitsCount'] = bits_count
+    f[path+'ImagesStack'].attrs['Format'] = qt_format
+    f[path+'ImagesStack'].attrs['Height'] = height
+    f[path+'ImagesStack'].attrs['Width'] = width
+
+
+def save_roi_signals(
+    signals: np.ndarray,
+    footprints: np.ndarray,
+    time_: np.array,
+    f: h5py.File,
+    path: str,
+    names: Optional[List[str]] = None,
+    usernames: Optional[List[str]] = None,
+    bits_count: int = 16,
+):
+
+    """
+    Saves ROI signals, time vector, and ROI coordinates.
+    Parameters
+    ----------
+    signals : np.ndarray
+        2D array of signals, with shape (n_ROI, time).
+    footprints:
+        3D array of spatial cell footprints with shape (n_ROI, height, width)
+    time_ : np.array
+        1D vector of timestamps
+    f : h5py.File
+        Opened HDF file where the information should be saved
+    path  : str
+        Group path in the HDF file
+    bits_count : int
+        Bits depth of images
+    qt_format : int
+        QImage_Format, necessary to display images in DNS. For reference, please
+        see https://doc.qt.io/qt-6/qimage.html
+
+    Returns
+    -------
+
+    Raises
+    ------
+
+    """
+
+    if path[-1] != '/':
+        path += '/'
+    
+    for i in range(len(footprints)):
+        coords = footprint_to_coords(footprints[i])
+        
+        attrs = {
+            'ID': i+1,
+            'Index': i+1,
+            'Name': 'ROI {}'.format(i+1),
+            'Username': 'ROI {}'.format(i+1),
+            'Shape': 0,
+            'BitsCount': bits_count,
+            'Coordinates': coords
+        }
+
+        if names is not None:
+            attrs['Name'] = names[i]
+
+        if usernames is not None:
+            attrs['Username'] = usernames[i]
+
+        dataset_name = 'ROI'+str(i+1).zfill(4)
+        
+        save_signal(signals[i], f, path+dataset_name, attrs)
+    
+    save_signal(time_, f, path+'Time')
+    
+
+def save_signal(
+    signal: np.array,
+    f: h5py.File, 
+    path: str,
+    attrs: Optional[dict] = None
+):
+    
+    try:
+        f.create_dataset(path, data=signal, dtype='float64', chunks=True, maxshape=None)
+    except:
+        del f[path]
+        f.create_dataset(path, data=signal, dtype='float64', chunks=True, maxshape=None)
+
+
+    if attrs is not None:
+        save_attributes(attrs, f, path)
+
+
+def save_signals(
+    signals: np.array,
+    time_: np.array,
+    f: h5py.File, 
+    path: str,
+    names: List[str],
+    usernames: Optional[List[str]] = None,
+    bits_count: Optional[int] = None,
+    range_min: Optional[float] = None,
+    range_max: Optional[float] = None,
+    unit: Optional[str] = "Intensity",
+):
+    
+    if bits_count is None and (range_min is None or range_max is None or unit is None):
+        raise ValueError('Set either bits count attribute, or range min, range max, and unit attributes.')
+    
+    if path[-1] != '/':
+        path += '/'
+    
+    try:
+        f.create_dataset(path+'Time', data=time_, dtype='float64', chunks=True, maxshape=None)
+    except:
+        pass
+    
+    for i,name in enumerate(names):
+        try:
+            f.create_dataset(path+name, data=signals[i], dtype='float64', chunks=True, maxshape=None)
+        except:
+            del f[path+name]
+            f.create_dataset(path+name, data=signals[i], dtype='float64', chunks=True, maxshape=None)
+        
+        attrs = {} 
+        
+        attrs['Username'] = usernames[i] if usernames is not None else name
+
+        if bits_count is not None:
+            attrs['BitsCount'] = bits_count
+        else:
+            attrs['RangeMin'] = range_min
+            attrs['RangeMax'] = range_max
+            attrs['Unit'] = unit
+
+        if attrs is not None:
+            save_attributes(attrs, f, path+name)
+
+
+def save_attributes(
+    attributes: dict,
+    f: h5py.File,
+    path: str
+):
+    
+    for key in attributes.keys():
+        try:
+            f[path].attrs[key] = attributes[key]
+        except:
+            print('Cannot save attribute {} with value {}'.format(key, attributes[key]))
+
+
+def footprint_to_coords(
+    footprint: np.array
+) -> np.array:
+    
+    _, mask = cv2.threshold(footprint, 0, 255, 0)
+    contours, _ = cv2.findContours(mask.astype('uint8'), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    maxI = 0
+    maxArea = 0
+    for i in range(len(contours)):
+        if cv2.contourArea(contours[i]) > maxArea:
+            maxArea = cv2.contourArea(contours[i])
+            maxI = i
+
+    coords = np.squeeze(contours[maxI])
+    
+    return coords
