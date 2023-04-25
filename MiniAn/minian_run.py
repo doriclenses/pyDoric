f45f6d24661ff0b3539df02f4203b38b629d2c1f
maximeB <maxime.baillot@doriclenses.com>
2023-04-25 15:59:09 -0400

    use sensor name as default username

--- MiniAn/minian_run.py
+++ MiniAn/minian_run.py
@@ -512,7 +512,7 @@ if __name__ == "__main__":
         fr=fr,
         bits_count=attrs['BitsCount'],
         qt_format=attrs['Format'],
-        imagesStackUsername=attrs['Username'] if 'Username' in attrs else 'MiniAn',
+        imagesStackUsername=attrs['Username'] if 'Username' in attrs else sensor,
         vname=params_load_doric['fname'], 
         vpath='DataProcessed/'+driver+'/',
         vdataset=series+'/'+sensor+'/',
