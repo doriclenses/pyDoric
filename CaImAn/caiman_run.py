bfefd06fbaad4ca645b450c4ecfc28e620e3980b
maximeB <maxime.baillot@doriclenses.com>
2023-06-28 16:26:54 -0400

    change for futur way of how danse will send the parameters to minian

--- CaImAn/caiman_run.py
+++ CaImAn/caiman_run.py
@@ -37,6 +37,7 @@ freeze_support()
 
 kwargs = {}
 params_doric = {}
+danse_parameters = {}
 
 try:
     for arg in sys.argv[1:]:
@@ -45,14 +46,18 @@ except SyntaxError:
     print_to_intercept("One of the advanced settings is not of a python type")
     sys.exit()
 
+if not danse_parameters:
+    danse_parameters = {"file_path": kwargs , "parameters": params_doric}
+
+file_path   = danse_parameters.get("file_path", {})
+parameters  = danse_parameters.get("parameters", {})
+
 tmpDir = tempfile.TemporaryDirectory(prefix="caiman_")
 tmpDirName = tmpDir.name
-fname = kwargs['fname']
-h5path = kwargs['h5path']
-fr = get_frequency(kwargs['fname'], kwargs['h5path']+'Time')
-dims, T = get_dims(kwargs['fname'], kwargs['h5path']+'ImagesStack')
+fr = get_frequency(file_path['fname'], file_path['h5path']+'Time')
+dims, T = get_dims(file_path['fname'], file_path['h5path']+'ImagesStack')
 
-neuron_diameter             = tuple([params_doric["NeuronDiameterMin"], params_doric["NeuronDiameterMax"]])
+neuron_diameter             = tuple([parameters["NeuronDiameterMin"], parameters["NeuronDiameterMax"]])
 
 params_caiman = {
     'fr': fr,
@@ -70,8 +75,8 @@ params_caiman = {
     'gSig': (neuron_diameter[0], neuron_diameter[0]),
     'merge_thr': 0.8,
     'p': 1,
-    'tsub': params_doric["TemporalDownsample"],
-    'ssub': params_doric["SpatialDownsample"],
+    'tsub': parameters["TemporalDownsample"],
+    'ssub': parameters["SpatialDownsample"],
     'rf': neuron_diameter[-1]*4,
     'only_init': True,    # set it to True to run CNMF-E
     'nb': 0,
@@ -79,8 +84,8 @@ params_caiman = {
     'method_deconvolution': 'oasis',       # could use 'cvxpy' alternatively
     'low_rank_background': None,
     'update_background_components': True,  # sometimes setting to False improve the results
-    'min_corr': params_doric["CorrelationThreshold"],
-    'min_pnr': params_doric["PNRThreshold"],
+    'min_corr': parameters["CorrelationThreshold"],
+    'min_pnr': parameters["PNRThreshold"],
     'normalize_init': False,               # just leave as is
     'center_psf': True,                    # leave as is for 1 photon
     'ssub_B': 2,
@@ -89,10 +94,7 @@ params_caiman = {
     'use_cnn': False
 }
 
-# extract advanced settings as variable advanced_settings
-advanced_settings = {}
-if "AdvancedSettings" in params_doric:
-    advanced_settings = params_doric["AdvancedSettings"]
+advanced_settings = parameters.get("AdvancedSettings", {})
 
 if __name__ == "__main__":
 
@@ -127,9 +129,9 @@ if __name__ == "__main__":
     opts = params.CNMFParams(params_dict=params_caiman)
     opts, advanced_settings = set_advanced_parameters(opts, advanced_settings)
     #Update AdvancedSettings
-    params_doric["AdvancedSettings"] = advanced_settings.copy()
+    parameters["AdvancedSettings"] = advanced_settings.copy()
 
-    if bool(params_doric["CorrectMotion"]):
+    if bool(parameters["CorrectMotion"]):
         # MOTION CORRECTION
         print("MOTION CORRECTION",  flush=True)
         # do motion correction rigid
@@ -167,6 +169,8 @@ if __name__ == "__main__":
 
     ### Save results to doric file ###
     print("Saving data to doric file...", flush=True)
+    # Get the path from the source data
+    h5path = file_path['h5path']
     if h5path[0] == '/':
         h5path = h5path[1:]
     if h5path[-1] == '/':
@@ -178,9 +182,9 @@ if __name__ == "__main__":
     series = h5path_names[-2]
     sensor = h5path_names[-1]
     # Get paramaters of the operation on source data
-    params_source_data = load_attributes(fname, data+'/'+driver+'/'+operation)
+    params_source_data = load_attributes(file_path['fname'], data+'/'+driver+'/'+operation)
     # Get the attributes of the images stack
-    attrs = load_attributes(fname, h5path+'/ImagesStack')
+    attrs = load_attributes(file_path['fname'], h5path+'/ImagesStack')
 
     # Parameters
     # Set only "Operations" for params_srouce_data
@@ -207,10 +211,10 @@ if __name__ == "__main__":
             bits_count=attrs['BitsCount'],
             qt_format=attrs['Format'],
             imagesStackUsername=attrs['Username'] if 'Username' in attrs else sensor,
-            vname=fname,
+            vname=file_path['fname'],
             vpath='DataProcessed/'+driver+'/',
             vdataset=series+'/'+sensor+'/',
-            params_doric = params_doric,
+            params_doric = parameters,
             params_source = params_source_data,
             saveimages=True,
             saveresiduals=True,
