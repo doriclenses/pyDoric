f4289b48db8ebdbdd038c49ee236b27f8ec80078
maximeB <maxime.baillot@doriclenses.com>
2023-09-19 17:02:28 -0400

    missing .Message.

--- MiniAn/minian_utilities.py
+++ MiniAn/minian_utilities.py
@@ -240,7 +240,7 @@ def except_type_error(function_name: str):
     try:
         yield
     except TypeError:
-        utils.print_to_intercept(mn_defs.ONE_PARM_WRONG_TYPE.format(function_name))
+        utils.print_to_intercept(mn_defs.Messages.ONE_PARM_WRONG_TYPE.format(function_name))
         sys.exit()
 
 @contextmanager
@@ -253,5 +253,5 @@ def except_print_error_no_cells(position: str):
         yield
     except Exception as error:
         utils.print_error(error, position)
-        utils.print_to_intercept(mn_defs.NO_CELLS_FOUND)
+        utils.print_to_intercept(mn_defs.Messages.NO_CELLS_FOUND)
         sys.exit()
