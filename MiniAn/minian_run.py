54c3ad3c59dd8fbf5c7abeb7ba52b115cc749672
maximeB <maxime.baillot@doriclenses.com>
2023-10-13 08:50:31 -0400

    transfer all the functions from minian_utilites to minian_main

--- MiniAn/minian_run.py
+++ MiniAn/minian_run.py
@@ -12,7 +12,6 @@ import xarray as xr
 sys.path.append('..')
 import utilities as utils
 import minian_main        as mn_main
-import minian_utilities   as mn_utils
 import minian_parameters  as mn_params
 import minian_definitions as mn_defs
 
