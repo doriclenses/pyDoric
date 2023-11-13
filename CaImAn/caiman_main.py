a127700f47ae2b86cfe9f63e038a5c483e84eb51
maximeB <maxime.baillot@doriclenses.com>
2023-11-13 15:58:10 -0500

    remove caiman_utilities and put all the functions in caiman_main

--- CaImAn/caiman_main.py
+++ CaImAn/caiman_main.py
@@ -2,10 +2,12 @@
 import os
 import sys
 import h5py
-
 import cv2
-import h5py
 import numpy as np
+import tempfile
+from tifffile import imwrite
+import logging
+from typing import Any, Dict, List, Optional, Tuple, Union, Callable
 
 # Import for CaimAn lib
 import caiman as cm
@@ -17,18 +19,13 @@ from caiman.motion_correction import MotionCorrect
 sys.path.append("..")
 # Import CaimAn related utilities libraries
 import utilities as utils
-import caiman_utilities   as cm_utils
 import caiman_definitions as cm_defs
 import caiman_parameters  as cm_params
-
-# Import miscellaneous libraries
-import tempfile
-from tifffile import imwrite
-import logging
 
 # Import for PyInstaller
 from multiprocessing import freeze_support
 freeze_support()
+
 
 def main(caiman_params):
 
@@ -73,7 +70,7 @@ def main(caiman_params):
     params_caiman['fnames'] = [fname_tif]
 
     opts = params.CNMFParams(params_dict=params_caiman)
-    opts, advanced_settings = cm_utils.set_advanced_parameters(opts, advanced_settings)
+    opts, advanced_settings = set_advanced_parameters(opts, advanced_settings)
     #Update AdvancedSettings
     parameters["AdvancedSettings"] = advanced_settings.copy()
 
@@ -153,7 +150,7 @@ def main(caiman_params):
     if len(cnm.estimates.C[cnm.estimates.idx_components,:]) == 0 :
         utils.print_to_intercept(cm_defs.Messages.NO_CELLS_FOUND)
     else :
-        cm_utils.save_caiman_to_doric(
+        save_caiman_to_doric(
             Yr,
             cnm.estimates.A[:,cnm.estimates.idx_components],
             cnm.estimates.C[cnm.estimates.idx_components,:],
@@ -173,3 +170,140 @@ def main(caiman_params):
             savespikes=True)
 
     cm.stop_server(dview=dview)
+
+
+def save_caiman_to_doric(
+    Y: np.ndarray,
+    A: np.ndarray,
+    C: np.ndarray,
+    S: np.ndarray,
+    shape,
+    fr: int,
+    bit_count: int,
+    qt_format: int,
+    imagesStackUsername: str,
+    vname: str = "caiman.doric",
+    vpath: str = "DataProcessed/MicroscopeDriver-1stGen1C/",
+    vdataset: str = 'Series1/Sensor1/',
+    params_doric: Optional[dict] = {},
+    params_source: Optional[dict] = {},
+    saveimages: bool = True,
+    saveresiduals: bool = True,
+    savespikes: bool = True
+) -> str:
+    """
+    Save CaImAn results to .doric file:
+    MiniAnImages - `AC` representing cellular activities as computed by :func:`minian.cnmf.compute_AtC`
+    MiniAnResidualImages - residule movie computed as the difference between `Y` and `AC`
+    MiniAnSignals - `C` with coordinates from `A`
+    MiniAnSpikes - `S`
+    Since the CNMF algorithm contains various arbitrary scaling process, a normalizing
+    scalar is computed with least square using a subset of frames from `Y` and `AC`
+    such that their numerical values matches.
+
+    """
+    ROISIGNALS = 'CaImAnROISignals'
+    IMAGES = 'CaImAnImages'
+    RESIDUALS = 'CaImAnResidualImages'
+    SPIKES = 'CaImAnSpikes'
+
+    print("generating traces")
+    AC = A.dot(C)
+    res = Y - AC
+
+    AC = AC.reshape(shape, order='F').transpose((-1, 0, 1))
+    res = res.reshape(shape, order='F').transpose((-1, 0, 1))
+    A = A.toarray()
+    A = A.reshape(shape[0],shape[1],A.shape[1], order='F').transpose((-1, 0, 1))
+
+    time_ = np.arange(0, shape[2]/fr, 1/fr, dtype='float64')
+
+    print("generating ROI names")
+    names = []
+    roiUsernames = []
+    for i in range(len(C)):
+        names.append('ROI'+str(i+1).zfill(4))
+        roiUsernames.append('ROI {}'.format(i+1))
+
+    with h5py.File(vname, 'a') as f:
+
+        # Check if CaImAn results already exist
+        operationCount = ''
+        if vpath in f:
+            operations = [ name for name in f[vpath] if ROISIGNALS in name ]
+            if len(operations) > 0:
+                operationCount = str(len(operations))
+                for operation in operations:
+                    operationAttrs = utils.load_attributes(f, vpath+operation)
+                    if utils.merge_params(params_doric, params_source) == operationAttrs:
+                        if(len(operation) == len(ROISIGNALS)):
+                            operationCount = ''
+                        else:
+                            operationCount = operation[-1]
+
+                        break
+
+
+        if vpath[-1] != '/':
+            vpath += '/'
+
+        if vdataset[-1] != '/':
+            vdataset += '/'
+
+        params_doric["Operations"] += operationCount
+
+        print("saving ROI signals")
+        pathROIs = vpath+ROISIGNALS+operationCount+'/'
+        utils.save_roi_signals(C, A, time_, f, pathROIs+vdataset, attrs_add={"RangeMin": 0, "RangeMax": 0, "Unit": "Intensity"})
+        utils.print_group_path_for_DANSE(pathROIs+vdataset)
+        utils.save_attributes(utils.merge_params(params_doric, params_source), f, pathROIs)
+
+        if saveimages:
+            print("saving images")
+            pathImages = vpath+IMAGES+operationCount+'/'
+            utils.save_images(AC, time_, f, pathImages+vdataset, bit_count=bit_count, qt_format=qt_format, username=imagesStackUsername)
+            utils.print_group_path_for_DANSE(pathImages+vdataset)
+            utils.save_attributes(utils.merge_params(params_doric, params_source, params_doric["Operations"] + "(Images)"), f, pathImages)
+
+        if saveresiduals:
+            print("saving residual images")
+            pathResiduals = vpath+RESIDUALS+operationCount+'/'
+            utils.save_images(res, time_, f, pathResiduals+vdataset, bit_count=bit_count, qt_format=qt_format, username=imagesStackUsername)
+            utils.print_group_path_for_DANSE(pathResiduals+vdataset)
+            utils.save_attributes(utils.merge_params(params_doric, params_source, params_doric["Operations"] + "(Residuals)"), f, pathResiduals)
+
+        if savespikes:
+            print("saving spikes")
+            pathSpikes = vpath+SPIKES+operationCount+'/'
+            utils.save_signals(S > 0, time_, f, pathSpikes+vdataset, names, roiUsernames, range_min=0, range_max=1)
+            utils.print_group_path_for_DANSE(pathSpikes+vdataset)
+            utils.save_attributes(utils.merge_params(params_doric, params_source), f, pathSpikes)
+
+    print("Saved to {}".format(vname))
+
+def set_advanced_parameters(
+    param,
+    advanced_parameters
+):
+
+    '''
+    input:
+    param: is a class CNMFParams
+    advanced_parameters: dict
+
+    ouput:
+    new param: is a class CNMFParams
+    new advanced_parameters: dict
+    '''
+    param_dict = param.to_dict()
+    advan_param_keys_used = []
+    for param_part_key, param_part_value in param_dict.items():
+        for part_key, part_value in param_part_value.items():
+            if part_key in advanced_parameters:
+               param_dict[param_part_key][part_key] = advanced_parameters[part_key]
+               advan_param_keys_used.append(part_key)
+
+    #keep only used keys in andvanced parameters
+    advanced_parameters = {key: advanced_parameters[key] for key in advan_param_keys_used}
+
+    return [param, advanced_parameters]
