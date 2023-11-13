0cb1cdf2d783e66cf075b38f2436ba213ceb1755
maximeB <maxime.baillot@doriclenses.com>
2023-11-13 12:37:46 -0500

    add classes in caiman_definitions and in caiman_parameters

---
+++ CaImAn/caiman_definitions.py
@@ -0,0 +1,32 @@
+
+class Messages:
+    """
+    Messages
+    """
+
+    _ = ""
+
+class Folder:
+    """
+    Folder Name
+    """
+
+    _ = ""
+
+class DoricFile:
+
+    """
+    Names of the .doric file structue
+    """
+
+    _ = ""
+
+
+class Preview:
+
+    """
+    Names of the HDF5 preview file structue
+    """
+
+    _ = ""
+
---
+++ CaImAn/caiman_parameters.py
@@ -0,0 +1,10 @@
+
+class CaimanParameters:
+
+    """
+    Parameters used in Caiman library
+    """
+
+    def __init__(self, danse_parameters):
+
+        print("Init", danse_parameters)
