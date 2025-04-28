63e62165af48d7769c3a771b2cadd58cafe30e4a
kapilseh <kapil.sehgal@doriclenses.com>
2025-04-28 15:50:26 -0400

    calculate meanImgE if not present

--- Suite2p/suite2p_main.py
+++ Suite2p/suite2p_main.py
@@ -106,6 +106,11 @@ def save_suite2p_to_doric(
 
     file_ = h5py.File(doric_file_name, 'w')
 
+    # add enhanced mean image
+    if "meanImgE" not in ops:
+        # ops["meanImgE"]  = suite2p.registration.compute_enhanced_mean_image(ops["meanImg"].astype(np.float32), ops)
+        ops = suite2p.extraction.enhanced_mean_image(ops)
+        
     # Check if Suite2p results already exist
     mean_image      = split_by_plane(ops['meanImg'], len(plane_IDs))
     median_filter   = split_by_plane(ops['meanImgE'], len(plane_IDs))
