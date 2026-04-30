2713480bd10535f29719ae801d08da1e9291f146
kapilseh <kapil.sehgal@doriclenses.com>
2026-04-30 15:52:44 -0400

    unitialize variable ( in case the fx needs to run without saveImages)

--- MiniAn/minian_main.py
+++ MiniAn/minian_main.py
@@ -902,6 +902,7 @@ def save_minian_to_doric(
     dataset_names = [defs.DoricFile.Dataset.ROI.format(str(id_).zfill(4)) for id_ in ids]
     usernames     = [defs.DoricFile.Dataset.ROI.format(id_) for id_ in ids]
 
+    images_datapath = ""
     with h5py.File(vname, 'a') as f:
 
         # Check if MiniAn results already exist
